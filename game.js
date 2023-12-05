819d6a2bbb3d5a4df6b7b478e442b6b51c77a58e
Bharat00007 <139622038+Bharat00007@users.noreply.github.com>
2023-12-05 21:38:32 +0530

    Update game.js

--- game.js
+++ game.js
@@ -12,3 +12,57 @@ document.addEventListener('DOMContentLoaded', () => {
         car.style.left = carPosition + '%';
     });
 });
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var player;
+var cursors;
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('car', 'path/to/your/car/image.png');
+    // Add more assets as needed
+}
+
+function create() {
+    player = this.physics.add.image(400, 300, 'car');
+    player.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+    } else {
+        player.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown) {
+        player.setVelocityY(-160);
+    } else if (cursors.down.isDown) {
+        player.setVelocityY(160);
+    } else {
+        player.setVelocityY(0);
+    }
+}
+
