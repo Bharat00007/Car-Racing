066a016ae0cd8c25c04909b38e853bdb56afecfb
Bharat00007 <139622038+Bharat00007@users.noreply.github.com>
2023-12-05 21:43:45 +0530

    Update game.js

--- game.js
+++ game.js
@@ -66,3 +66,136 @@ function update() {
     }
 }
 
+
+var config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var player;
+var cursors;
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('car', 'path/to/your/car/image.png');
+    // Add more assets as needed
+}
+
+function create() {
+    player = this.physics.add.image(config.width / 2, config.height / 2, 'car');
+    player.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.input.addPointer();
+}
+
+function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+    } else {
+        player.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown) {
+        player.setVelocityY(-160);
+    } else if (cursors.down.isDown) {
+        player.setVelocityY(160);
+    } else {
+        player.setVelocityY(0);
+    }
+
+    // Mobile touch input
+    if (this.input.pointer1.isDown) {
+        var touchX = this.input.pointer1.x;
+        var touchY = this.input.pointer1.y;
+
+        var angle = Phaser.Math.Angle.Between(player.x, player.y, touchX, touchY);
+        var speed = 160;
+
+        player.setVelocityX(Math.cos(angle) * speed);
+        player.setVelocityY(Math.sin(angle) * speed);
+    }
+}
+var config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var player;
+var cursors;
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('car', 'path/to/your/car/image.png');
+    // Add more assets as needed
+}
+
+function create() {
+    player = this.physics.add.image(config.width / 2, config.height / 2, 'car');
+    player.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.input.addPointer();
+}
+
+function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+    } else {
+        player.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown) {
+        player.setVelocityY(-160);
+    } else if (cursors.down.isDown) {
+        player.setVelocityY(160);
+    } else {
+        player.setVelocityY(0);
+    }
+
+    // Mobile touch input
+    if (this.input.pointer1.isDown) {
+        var touchX = this.input.pointer1.x;
+        var touchY = this.input.pointer1.y;
+
+        var angle = Phaser.Math.Angle.Between(player.x, player.y, touchX, touchY);
+        var speed = 160;
+
+        player.setVelocityX(Math.cos(angle) * speed);
+        player.setVelocityY(Math.sin(angle) * speed);
+    }
+}
