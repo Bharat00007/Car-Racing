00ba0f52fdfc6d8b5fade5d9f4f3598aee41fea5
Bharat00007 <139622038+Bharat00007@users.noreply.github.com>
2023-12-06 07:55:35 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,201 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const car = document.getElementById('car');
-    let carPosition = 50; // Initial position
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
-    document.addEventListener('keydown', (event) => {
-        if (event.code === 'ArrowLeft' && carPosition > 0) {
-            carPosition -= 10;
-        } else if (event.code === 'ArrowRight' && carPosition < 90) {
-            carPosition += 10;
-        }
+// Car object
+const car = {
+  x: 100,
+  y: 300,
+  width: 50,
+  height: 50,
+  speed: 5,
+  draw: function() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  },
+  moveLeft: function() {
+    this.x -= this.speed;
+  },
+  moveRight: function() {
+    this.x += this.speed;
+  }
+};
 
-        car.style.left = carPosition + '%';
-    });
+// Game loop
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  car.draw();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
+// Event listeners for car movement
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowLeft') {
+    car.moveLeft();
+  } else if (event.key === 'ArrowRight') {
+    car.moveRight();
+  }
 });
-
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
-
-var player;
-var cursors;
-
-var game = new Phaser.Game(config);
-
-function preload() {
-    this.load.image('car', 'path/to/your/car/image.png');
-    // Add more assets as needed
-}
-
-function create() {
-    player = this.physics.add.image(400, 300, 'car');
-    player.setCollideWorldBounds(true);
-
-    cursors = this.input.keyboard.createCursorKeys();
-}
-
-function update() {
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
-    } else {
-        player.setVelocityX(0);
-    }
-
-    if (cursors.up.isDown) {
-        player.setVelocityY(-160);
-    } else if (cursors.down.isDown) {
-        player.setVelocityY(160);
-    } else {
-        player.setVelocityY(0);
-    }
-}
-
-
-var config = {
-    type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
-
-var player;
-var cursors;
-
-var game = new Phaser.Game(config);
-
-function preload() {
-    this.load.image('car', 'path/to/your/car/image.png');
-    // Add more assets as needed
-}
-
-function create() {
-    player = this.physics.add.image(config.width / 2, config.height / 2, 'car');
-    player.setCollideWorldBounds(true);
-
-    cursors = this.input.keyboard.createCursorKeys();
-
-    this.input.addPointer();
-}
-
-function update() {
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
-    } else {
-        player.setVelocityX(0);
-    }
-
-    if (cursors.up.isDown) {
-        player.setVelocityY(-160);
-    } else if (cursors.down.isDown) {
-        player.setVelocityY(160);
-    } else {
-        player.setVelocityY(0);
-    }
-
-    // Mobile touch input
-    if (this.input.pointer1.isDown) {
-        var touchX = this.input.pointer1.x;
-        var touchY = this.input.pointer1.y;
-
-        var angle = Phaser.Math.Angle.Between(player.x, player.y, touchX, touchY);
-        var speed = 160;
-
-        player.setVelocityX(Math.cos(angle) * speed);
-        player.setVelocityY(Math.sin(angle) * speed);
-    }
-}
-var config = {
-    type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
-
-var player;
-var cursors;
-
-var game = new Phaser.Game(config);
-
-function preload() {
-    this.load.image('car', 'path/to/your/car/image.png');
-    // Add more assets as needed
-}
-
-function create() {
-    player = this.physics.add.image(config.width / 2, config.height / 2, 'car');
-    player.setCollideWorldBounds(true);
-
-    cursors = this.input.keyboard.createCursorKeys();
-
-    this.input.addPointer();
-}
-
-function update() {
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
-    } else {
-        player.setVelocityX(0);
-    }
-
-    if (cursors.up.isDown) {
-        player.setVelocityY(-160);
-    } else if (cursors.down.isDown) {
-        player.setVelocityY(160);
-    } else {
-        player.setVelocityY(0);
-    }
-
-    // Mobile touch input
-    if (this.input.pointer1.isDown) {
-        var touchX = this.input.pointer1.x;
-        var touchY = this.input.pointer1.y;
-
-        var angle = Phaser.Math.Angle.Between(player.x, player.y, touchX, touchY);
-        var speed = 160;
-
-        player.setVelocityX(Math.cos(angle) * speed);
-        player.setVelocityY(Math.sin(angle) * speed);
-    }
-}
